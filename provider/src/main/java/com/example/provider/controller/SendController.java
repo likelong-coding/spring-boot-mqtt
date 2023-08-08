4da37078ad4c48fe10b76655a9c47f12e8371853
likelong <likelonglkl@163.com>
2023-08-08 18:20:06 +0800

    feat: MQTT简易Demo：发送者和消费者简易实现

---
+++ provider/src/main/java/com/example/provider/controller/SendController.java
@@ -0,0 +1,26 @@
+package com.example.provider.controller;
+
+
+import com.example.provider.config.MqttProviderConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SendController {
+    @Autowired
+    private MqttProviderConfig providerClient;
+
+    @RequestMapping("/sendMessage")
+    @ResponseBody
+    public String sendMessage(int qos, boolean retained, String topic, String message) {
+        try {
+            providerClient.publish(qos, retained, topic, message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "发送失败";
+        }
+    }
+}
